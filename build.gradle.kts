003b78cd1f0e282e934d940396e5dba6f768b485
Destiny Amba <destiny.amba@gmail.com>
2025-04-06 18:53:40 +0100

    refactor: update command

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.6"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
+	id("org.sonarqube") version "4.4.1.3373"
 }
 
 tasks.bootJar {
@@ -102,4 +103,12 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sonarqube {
+	properties {
+		property("sonar.projectKey", "your-project-key")
+		property("sonar.organization", "your-organization")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
